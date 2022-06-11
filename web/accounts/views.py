ab65bea00a95302d0f58490738d8d294bd3128b3
kostadinov1 <evgenikostadinov1987@gmail.com>
2022-06-11 23:44:44 +0300

    added logout

--- web/accounts/views.py
+++ web/accounts/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics as api_view
+from rest_framework import views as rest_views
 from rest_framework.authtoken import views as auth_views
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
 from web.accounts.serizlizers import CreateUserSerializer
 
 
@@ -16,5 +20,16 @@ class LoginView(auth_views.ObtainAuthToken):
     pass
 
 
-class LogoutView(api_view.CreateAPIView):
-    pass
+class LogoutView(rest_views.APIView):
+
+    @staticmethod
+    def __perform_logout(request):
+        token = Token.objects.get(user=request.user)
+        token.delete()
+        return Response({'message': 'User Has Logged Out!'})
+
+    def get(self, request):
+        return self.__perform_logout(request)
+
+    def post(self, request):
+        return self.__perform_logout(request)
