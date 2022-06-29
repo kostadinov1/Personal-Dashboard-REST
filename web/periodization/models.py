bd3e109fb3b11ceab2707e3099618c4f0060f2fb
kostadinov1 <evgenikostadinov1987@gmail.com>
2022-06-29 23:40:24 +0300

    modified activity and periodization models

--- web/periodization/models.py
+++ web/periodization/models.py
@@ -2,55 +2,65 @@ from django.db import models
 
 
 class Goal(models.Model):
-    name = models.CharField(max_length=40, blank=False, null=False)
+    NAME_MAX_LEN = 50
+
+    name = models.CharField(max_length=NAME_MAX_LEN, blank=False, null=False)
     description = models.TextField(blank=False, null=False)
     deadline_date = models.DateField(blank=False, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
 
 
 class Defender(models.Model):
-    name = models.CharField(max_length=40, blank=False, null=False)
+    NAME_MAX_LEN = 50
+
+    name = models.CharField(max_length=NAME_MAX_LEN, blank=False, null=False)
     description = models.TextField(blank=False, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
 
 
+# MICRO DURATION = 1 WEEK
 class MicroCycle(models.Model):
-    # MICRO DURATION = 1 WEEK
+    NAME_MAX_LEN = 50
 
-    name = models.CharField(max_length=30, blank=False, null=False, default='Micro Cycle')
+    name = models.CharField(max_length=NAME_MAX_LEN, blank=False, null=False, default='Micro Cycle')
     start_date = models.DateField(blank=False, null=False)
     end_date = models.DateField(blank=False, null=False)
     description = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    block_name = models.CharField(max_length=30, blank=True, null=True)
+
     goals = models.ForeignKey(Goal, on_delete=models.CASCADE)
     defenders = models.ForeignKey(Defender, on_delete=models.CASCADE)
-    block_name = models.CharField(max_length=30, blank=True, null=True)
     activities = []
     meals = []
 
 
+# MESO DURATION =  1-4 MONTHS
 class MesoCycle(models.Model):
-    # MESO DURATION =  1-4 MONTHS
+    NAME_MAX_LEN = 50
 
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=NAME_MAX_LEN, blank=False, null=False, default='Meso Cycle')
     start_date = models.DateField(blank=False, null=False)
     end_date = models.DateField(blank=False, null=False)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    micro_cycles = models.ForeignKey(MicroCycle, on_delete=models.CASCADE)
 
+    micro_cycles = models.ForeignKey(MicroCycle, on_delete=models.CASCADE)
     goals = models.ForeignKey(Goal, on_delete=models.CASCADE)
     defenders = models.ForeignKey(Defender, on_delete=models.CASCADE)
 
 
+# MACRO DURATION = 1 YEAR
 class MacroCycle(models.Model):
-    # MACRO DURATION = 1 YEAR
+    NAME_MAX_LEN = 50
 
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=NAME_MAX_LEN, blank=False, null=False, default='Macro Cycle')
     start_date = models.DateField(blank=False, null=False)
     end_date = models.DateField(blank=False, null=False)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    meso_cycles = models.ForeignKey(MesoCycle, on_delete=models.CASCADE)
 
+    meso_cycles = models.ForeignKey(MesoCycle, on_delete=models.CASCADE)
     goals = models.ForeignKey(Goal, on_delete=models.CASCADE)
     defenders = models.ForeignKey(Defender, on_delete=models.CASCADE)
 
--- web/physical_activities/models.py
+++ web/physical_activities/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from web.periodization.models import Goal
+
 
 class ActivityType(models.Model):
     NAME_MAX_LEN = 30
 
-    name = models.CharField(max_length=NAME_MAX_LEN)
+    name = models.CharField(max_length=NAME_MAX_LEN, null=False, blank=False, default='Physical')
     description = models.TextField(null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
 
@@ -12,38 +14,43 @@ class ActivityType(models.Model):
 class Activity(models.Model):
     NAME_MAX_LEN = 30
 
-    name = models.CharField(max_length=NAME_MAX_LEN)
-    type = models.ForeignKey(ActivityType, on_delete=models.CASCADE)
-
+    name = models.CharField(max_length=NAME_MAX_LEN, null=False, blank=False, default='Activity')
     description = models.TextField(null=True, blank=True)
-
     created_on = models.DateTimeField(auto_now_add=True)
+
+    # ENDURANCE METRICS (WORKOUT TARGETS)
+    distance = models.PositiveIntegerField(null=True, blank=True)
+    pace = models.PositiveIntegerField(null=True, blank=True)
+    speed = models.PositiveIntegerField(null=True, blank=True)
+    rpe = models.PositiveIntegerField(null=True, blank=True)
+
+    type = models.ForeignKey(ActivityType, on_delete=models.CASCADE)
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
 
 
 class ExerciseType(models.Model):
     NAME_MAX_LEN = 30
 
-    name = models.CharField(max_length=NAME_MAX_LEN)
+    name = models.CharField(max_length=NAME_MAX_LEN, null=False, blank=False, default='General')
     description = models.TextField(null=True, blank=True)
-
     created_on = models.DateTimeField(auto_now_add=True)
 
 
 class Exercise(models.Model):
     NAME_MAX_LEN = 30
 
-    name = models.CharField(max_length=NAME_MAX_LEN)
-    type = models.ForeignKey(ExerciseType, on_delete=models.DO_NOTHING, null=True, blank=True)
+    name = models.CharField(max_length=NAME_MAX_LEN, null=False, blank=False, default='Movement')
     description = models.TextField(null=True, blank=True)
-    tempo = []
-
-    calories_burned = models.PositiveIntegerField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
     created_on = models.DateTimeField(auto_now_add=True)
 
+    # BODYBUILDING METRICS
+    reps = models.PositiveIntegerField(null=True, blank=True)
+    weights_in_kg = models.PositiveIntegerField(null=True, blank=True)
+    tempo = []
+    calories_burned = models.PositiveIntegerField(null=True, blank=True)
+    break_in_seconds = models.PositiveIntegerField(null=True, blank=True)
 
-class Break(models.Model):
-    name = models.CharField(null=True, blank=True)
-    duration = models.PositiveIntegerField(null=False, blank=False)
-    description = models.TextField(null=True, blank=True)
+    type = models.ForeignKey(ExerciseType, on_delete=models.DO_NOTHING, null=True, blank=True)
+
+
